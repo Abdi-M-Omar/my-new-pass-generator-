ae2e9f4fcd37d4539438ca85c896cdd90c74c0d2
Abdi Omar <abdiharbi47@gmail.com>
2023-11-16 14:06:58 -0600

    script file modified

--- Develop/script.js
+++ Develop/script.js
@@ -10,8 +10,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+// Function to generate a random password
+function generatePassword() {
+  // Define characters that can be used in the password
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var numberChars = "0123456789";
+  var specialChars = "!@#$%^&*()_-+=";
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // Combine all characters into a single string
+  var allChars = uppercaseChars + lowercaseChars + numberChars + specialChars;
+
+  // Prompt user for password length
+  var passwordLength = parseInt(prompt("Enter password length (between 8 and 128):"));
+
+  // Validate password length
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length between 8 and 128.");
+    return "";
+  }  
